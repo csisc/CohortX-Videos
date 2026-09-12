b2c5f39f72608afbc3103870cdb0ebde4995ef64
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2026-09-12 12:05:43 +0100

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -4,20 +4,20 @@ export interface Video {
 }
 
 export const videos: Video[] = [
-  { id: "cg8BxySmWhQ", title: "Task1 presentation CohortX MICCAI 2026" },
-  { id: "C5hirHINAWA", title: "CohortX challenge, Task 2" },
-  { id: "zh4QTKDURBY", title: "CohortX challenge Task 3" },
-  { id: "bRbAG1oXnHE", title: "MICCAI 2026 - Cohort X - Task1" },
-  { id: "lHanxUeHmQw", title: "MICCAI 2026 - Cohort X - Task2" },
-  { id: "p8a9Mwu0gkQ", title: "MICCAI 2026 - Cohort X - Task3" },
-  { id: "Koi8_58oqLA", title: "CohortX Task 2 & Task 3 | FAU | MICCAI 2026 Challenge Showcase" },
-  { id: "4fHRuKunkzc", title: "CohortX Task 2 – Structuring Cohort Eligibility Criteria as Triples | MICCAI 2026 | Yingfa Li" },
-  { id: "Ud1lJv4e6LE", title: "CohortX Task 3 – Resolving Medical Conditions to ICD-10-CM Codes | MICCAI 2026 | Yingfa Li" },
-  { id: "hFGFjrupG3E", title: "CohortX Task 2 @ MICCAI 2026 - Ngọc Hưng Nguyễn" },
-  { id: "xngmAC_BlJc", title: "CohortX Task 1 (MICCAI 2026) – Learned Passage Ranking for Cohort Criteria Extraction" },
-  { id: "2Ruj-qZYzWs", title: "cohortx task2 explainer" },
-  { id: "B_a74MaB2Kk", title: "CohortX Task 2 @ MICCAI 2026 - Mohit" },
-  { id: "UeYyldMYN4g", title: "CohortX_Task2" },
-  { id: "5tXFkvbtqD0", title: "CohortX task2 Flan solution" },
-  { id: "6BF38y1ltgI", title: "Monash AIM CohortX Showcase" }
+  { id: "cg8BxySmWhQ", title: "NTUA - CohortX Task 1" },
+  { id: "C5hirHINAWA", title: "NTUA - CohortX Task 2" },
+  { id: "zh4QTKDURBY", title: "NTUA - CohortX Task 3" },
+  { id: "bRbAG1oXnHE", title: "Alan T. Andrea - CohortX Task 1" },
+  { id: "lHanxUeHmQw", title: "Alan T. Andrea - CohortX Task 2" },
+  { id: "p8a9Mwu0gkQ", title: "Alan T. Andrea - CohortX Task 3" },
+  { id: "Koi8_58oqLA", title: "Md Raihan - CohortX Task 2 and 3" },
+  { id: "4fHRuKunkzc", title: "Yingfa Li - CohortX Task 2" },
+  { id: "Ud1lJv4e6LE", title: "Yingfa Li - CohortX Task 3" },
+  { id: "hFGFjrupG3E", title: "Ngọc Hưng Nguyễn - CohortX Task 2 and 3" },
+  { id: "xngmAC_BlJc", title: "Sanket Kachole - CohortX Task 1" },
+  { id: "2Ruj-qZYzWs", title: "Jason Karpeles - CohortX Task 2" },
+  { id: "B_a74MaB2Kk", title: "Mohit - CohortX Task 2" },
+  { id: "UeYyldMYN4g", title: "Harsh Rajbhar - CohortX Task 2" },
+  { id: "5tXFkvbtqD0", title: "Flan - CohortX Task 2" },
+  { id: "6BF38y1ltgI", title: "Monash AIM - CohortX Task 3" }
 ];
